d2d31e9d59598e75e572dcd53cefe4e884d51eab
Gustavo Magnago <ext.gustavo.magnago@farfetch.com>
2025-04-15 17:27:33 +0300

    chore: rename Select → select and Agenda → agenda

--- src/components/Agenda/components/confirmation-modal/ConfirmationModal.types.ts
+++
@@ -1,7 +0,0 @@
-export interface IConfirmationModal {
-	availabilityId: string;
-	handleCancelConfirmation: () => void;
-	openConfirmationModal: boolean;
-	selectedSlot: Date | null;
-	therapistId: string;
-}
